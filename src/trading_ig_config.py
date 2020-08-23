91c865a4a10324ce158dbadcec4ec795b8fe3de2
Alistair Hennessey <al@Alistairs-MBP.home>
2020-08-23 22:31:25 +0100

    23/08

---
+++ src/trading_ig_config.py
@@ -0,0 +1,6 @@
+class config(object):
+    username = "alhennessey92"
+    password = "dabXY654@"
+    api_key = "45a949a3b92595e16924208c9a58c32e184c606b"
+    acc_type = "DEMO"  # LIVE / DEMO
+    acc_number = "Z2UDF"
